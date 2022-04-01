aa019ef17e736c6950aacd39c66e7a292cf4677c
Namig1991 <safarovnamig09@gmail.com>
2022-04-01 13:59:54 +0300

    Мой первый коммит.

---
+++ src/main/java/pro/skyjavanamigemployeebook/skyproemployeebooknamig/SkyproEmployeebookNamigApplication.java
@@ -0,0 +1,13 @@
+package pro.skyjavanamigemployeebook.skyproemployeebooknamig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeebookNamigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeebookNamigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/skyjavanamigemployeebook/skyproemployeebooknamig/SkyproEmployeebookNamigApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skyjavanamigemployeebook.skyproemployeebooknamig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproEmployeebookNamigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
